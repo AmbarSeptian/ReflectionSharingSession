00bf7d1dee43d6ff92722fba2d4eedb9890d9ba6
Ambar Septian <ambar.septian@tokopedia.com>
2021-01-13 21:35:43 +0700

    Initial Commit

---
+++ ReflectionSharingSession/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReflectionSharingSession
+//
+//  Created by Ambar Septian on 13/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReflectionSharingSession/ReflectionSharingSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectionSharingSessionApp.swift
+//  ReflectionSharingSession
+//
+//  Created by Ambar Septian on 13/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ReflectionSharingSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
